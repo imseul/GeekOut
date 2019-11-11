9b93479c96564d4a57dfa289187557fc4b88e0bb
RiteshPachgade <ritesh@terpmail.umd.edu>
2019-11-11 18:46:35 -0500

    UI Interaction
    UI interaction for Answer, Bidding, MainActivity, and RollDice

--- app/src/main/java/com/example/geek_out/Answer.kt
+++ app/src/main/java/com/example/geek_out/Answer.kt
@@ -1,9 +1,37 @@
 package com.example.geek_out
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
 class Answer:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.answer)
+
+        val submitButton = findViewById<Button>(R.id.submitAnswer)
+        val resetButton = findViewById<Button>(R.id.resetAnswer)
+
+        submitButton.setOnClickListener {
+            getResponse()
+        }
+
+        resetButton.setOnClickListener {
+            resetResponse()
+        }
+    }
+
+    fun getResponse() {
+        val answer = findViewById<EditText>(R.id.answerText).text.toString()
+
+        //  Processing response
+
+    }
+
+    fun resetResponse() {
+        findViewById<EditText>(R.id.answerText).getText().clear()
+
+        //  Reset response from database
+
     }
 }
