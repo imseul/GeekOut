79f3977637d10a0e139066803ee820aac10f3029
Kemi Lin <kemilin@wireless-10-105-173-235.umd.edu>
2019-12-03 14:33:51 -0500

    adding winner page

---
+++ app/src/main/java/com/example/geek_out/Winning.kt
@@ -0,0 +1,11 @@
+package com.example.geek_out
+import android.app.Activity
+import android.os.Bundle
+
+class Winning: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.winninguser)
+        //need to set the winning user in the textview
+    }
+}
